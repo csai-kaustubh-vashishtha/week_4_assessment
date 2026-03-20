4a5dace169b641648ac0c8bf8870369f55fcc3e3
csai-kaustubh-vashishtha <csai.kaustubh.vashishtha@gmail.com>
2026-03-20 12:31:26 +0530

    Question-1

---
+++ Online-Food-Order-Management-System/src/main/java/com/capgemini/repository/OrderRepository.java
@@ -0,0 +1,8 @@
+package com.capgemini.repository;
+
+
+import com.capgemini.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+}
